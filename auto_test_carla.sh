9081ac8e75cbaa4b3b11bb903b4ebbd02a4c1e18
wuhanstudio <wuhanstudio@qq.com>
2024-05-22 15:47:34 +0100

    Update test script

--- auto_test_carla.sh
+++ auto_test_carla.sh
@@ -1,17 +1,27 @@
 for VIDEO in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
 do
+    # Ground Truth
     python 2d-tracking-gt.py --video ${VIDEO} --dataset carla
 
+    # Ground Truth as Detector
     python 2d-tracking-gt-sort.py --video ${VIDEO} --dataset carla
     python 2d-tracking-gt-deep-sort.py --video ${VIDEO} --dataset carla
     python 2d-tracking-gt-oc-sort.py --video ${VIDEO} --dataset carla
     python 2d-tracking-gt-strong-sort.py --video ${VIDEO} --dataset carla
 
-    python 2d-tracking-yolo-sort.py --video ${VIDEO} --dataset carla
-    python 2d-tracking-yolo-deep-sort.py --video ${VIDEO} --dataset carla
-    python 2d-tracking-yolo-oc-sort.py --video ${VIDEO} --dataset carla
-    python 2d-tracking-yolo-strong-sort.py --video ${VIDEO} --dataset carla
+    # Yolov3 as Detector
+    python 2d-tracking-yolov3-sort.py --video ${VIDEO} --dataset carla
+    python 2d-tracking-yolov3-deep-sort.py --video ${VIDEO} --dataset carla
+    python 2d-tracking-yolov3-oc-sort.py --video ${VIDEO} --dataset carla
+    python 2d-tracking-yolov3-strong-sort.py --video ${VIDEO} --dataset carla
 
+    # Yolov4 as Detector
+    python 2d-tracking-yolov4-sort.py --video ${VIDEO} --dataset carla
+    python 2d-tracking-yolov4-deep-sort.py --video ${VIDEO} --dataset carla
+    python 2d-tracking-yolov4-oc-sort.py --video ${VIDEO} --dataset carla
+    python 2d-tracking-yolov4-strong-sort.py --video ${VIDEO} --dataset carla
+
+    # YoloX as Detector
     python 2d-tracking-yolox-sort.py --video ${VIDEO} --dataset carla
     python 2d-tracking-yolox-deep-sort.py --video ${VIDEO} --dataset carla
     python 2d-tracking-yolox-oc-sort.py --video ${VIDEO} --dataset carla
